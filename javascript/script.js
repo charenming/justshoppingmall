44029ee8c9ea65ce53f5422719d65d4e5d370bed
charenming <ysem88@gmail.com>
2021-08-29 02:54:30 +0900

    justshop

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,13 @@
 $(function(){
 
+    $(".gnb>li").mouseover(function(){
+        $(this).children(".subMenu").stop().slideDown();
+    });
+    $(".gnb>li").mouseleave(function(){
+        $(this).children(".subMenu").stop().slideUp();
+    });
+
+
     let currentIndex=0;
     setInterval(function(){
         if(currentIndex<2){
@@ -10,4 +18,4 @@ $(function(){
         let slidePosition=currentIndex*(-300)+"px";
         $('.slide').animate({top:slidePosition},300);
     },3000);
-})
+});
